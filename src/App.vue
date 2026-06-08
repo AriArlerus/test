0be681ea661d42d9d3228be57511b74bafe199db
ARI ARLERUS <108457826+AriArlerus@users.noreply.github.com>
2026-06-08 11:26:40 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <v-btn
+        color="primary"
+        style="position: fixed; top: 16px; right: 16px; z-index: 100;"
+        @click="$refs.addDialog.open()"
+      >
+        + เพิ่มข้อมูล
+      </v-btn>
+
+      <AddDialog ref="addDialog" @saved="$refs.incomponents.fetchData()" />
+      <Incomponents ref="incomponents" />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Incomponents from './components/Incomponents.vue'
+import AddDialog from './components/Add.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Incomponents,
+    AddDialog,
+  },
 }
 </script>
 
@@ -19,8 +33,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
